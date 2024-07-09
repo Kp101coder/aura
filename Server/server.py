e8572c0d6bb1a760a9b970beb655f347e8253ba0
D Krish Prabhu <prabhukrish710@gmail.com>
2024-07-08 19:38:40 -0500

    Cleanup
    Finished basic server program

--- Server/server.py
+++ Server/server.py
@@ -2,5 +2,17 @@ import socket as s
 
 HOST = s.gethostbyname(s.gethostname())
 PORT = 7106
+MAX_BYTES_ACCEPTED = 1024
 
 server = s.socket(s.AF_INET, s.SOCK_STREAM)
+server.bind((HOST,PORT))
+
+server.listen(100)
+
+while True:
+    communication_socket, address = server.accept()
+    print(f"Connected to {address} on {communication_socket}")
+    message = communication_socket.recv(MAX_BYTES_ACCEPTED).decode('utf-8')
+    print(f"Message from client is: {message}")
+    communication_socket.send(f"Hello!".encode('utf-8'))
+    communication_socket.close()
