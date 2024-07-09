27a9a7e1f638a68408ebef328d3e7ce68c3efd1f
D Krish Prabhu <prabhukrish710@gmail.com>
2024-07-09 00:48:46 -0500

    Update server.py

--- Server/server.py
+++ Server/server.py
@@ -1,5 +1,12 @@
-wget "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
-bash Miniforge3-$(uname)-$(uname -m).sh
+sudo apt install -y build-essential libssl-dev zlib1g-dev libncurses5-dev libncursesw5-dev libreadline-dev libsqlite3-dev libgdbm-dev libdb5.3-dev libbz2-dev libexpat1-dev liblzma-dev tk-dev libffi-dev
+cd /tmp
+wget https://www.python.org/ftp/python/3.11.9/Python-3.11.9.tgz
+tar -xvf Python-3.11.9.tgz
+cd Python-3.11.9
+./configure --enable-optimizations
+make -j$(nproc)
+sudo make altinstall
+python3.11 --version
 import socket as s
 import PIL.Image as Image
 import io
