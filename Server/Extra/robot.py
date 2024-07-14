12132a9004ac9fb9af3481466030b23e1e7707e8
D Krish Prabhu <prabhukrish710@gmail.com>
2024-07-14 18:13:55 -0500

    Added network connection for server
    Sometimes, the raspberry pi server forgets the network on bootup and does not connect automatically. Added functionality to connect when checking for update, the first time network is needed.

--- Server/Extra/robot.py
+++ Server/Extra/robot.py
@@ -22,6 +22,42 @@ while(True):
 
 SCOPES = ['https://mail.google.com/', 'https://www.googleapis.com/auth/drive']
 
+def createNewConnection(name, SSID, password):
+	config = """<?xml version=\"1.0\"?>
+<WLANProfile xmlns="http://www.microsoft.com/networking/WLAN/profile/v1">
+	<name>"""+name+"""</name>
+	<SSIDConfig>
+		<SSID>
+			<name>"""+SSID+"""</name>
+		</SSID>
+	</SSIDConfig>
+	<connectionType>ESS</connectionType>
+	<connectionMode>auto</connectionMode>
+	<MSM>
+		<security>
+			<authEncryption>
+				<authentication>WPA2PSK</authentication>
+				<encryption>AES</encryption>
+				<useOneX>false</useOneX>
+			</authEncryption>
+			<sharedKey>
+				<keyType>passPhrase</keyType>
+				<protected>false</protected>
+				<keyMaterial>"""+password+"""</keyMaterial>
+			</sharedKey>
+		</security>
+	</MSM>
+</WLANProfile>"""
+	command = "netsh wlan add profile filename=\""+name+".xml\""+" interface=Wi-Fi"
+	with open(name+".xml", 'w') as file:
+		file.write(config)
+	os.system(command)
+
+# function to connect to a network 
+def connect(name, SSID):
+	command = "netsh wlan connect name=\""+name+"\" ssid=\""+SSID+"\" interface=Wi-Fi"
+	os.system(command)
+
 def search_for_file(service, name, type, parents):
     print("Searching for File")
     #application/vnd.google-apps.folder
@@ -83,6 +119,9 @@ def mouseKeyboard():
     keyboard.type("python3.11 server.py")
     keyboard.press(Key.enter)
     keyboard.release(Key.enter)
+
+createNewConnection("Prabhu", "Prabhu", "7136793536")
+connect("Prabhu", "Prabhu")
 
 creds = None
 try:
