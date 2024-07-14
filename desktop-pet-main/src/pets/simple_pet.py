5cec60a876e7a4deb50df2201b05ecce7399d8fc
Andy Wei <andywei2006@gmail.com>
2024-07-13 19:56:48 -0500

    changing some graphics

--- desktop-pet-main/src/pets/simple_pet.py
+++ desktop-pet-main/src/pets/simple_pet.py
@@ -73,10 +73,19 @@ class SimplePet:
         
         """Update the window position and scale to match that of the pet instance's location and size"""
         size = self.animator.animations[self.animator.state].target_resolution
+<<<<<<< Updated upstream
         self.canvas.window.geometry("200x200")
         # self.canvas.window.geometry(
         #     str(200) + "x" + str(200) + "+" + str(self.x) + "+" + str(self.y)
         # )
+=======
+        print(size[0])
+        print(size[1])
+        
+        self.canvas.window.geometry(
+            str(size[0]) + "x" + str(size[1]) + "+" + str(self.x) + "+" + str(self.y)
+        )
+>>>>>>> Stashed changes
 
     def handle_event(self):
         """Part of animation loop, after delay between frames in animation
