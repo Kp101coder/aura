dc167b08f83432102d5e5c7e7a35ce467e62b0c4
D Krish Prabhu <prabhukrish710@gmail.com>
2024-07-20 21:48:34 -0500

    Add shutdown

--- AuraBuddy/src/ActionHandler.py
+++ AuraBuddy/src/ActionHandler.py
@@ -1,9 +1,15 @@
 from .animation import AnimationStates
-import time
+import os
 
 class ActionHandler():
     def handle(self,response):
-        if(response.get('action') == "Play Gif"):
-            if(response.get('code') == "Treat"):
+        action = response.get('action')
+        code = response.get('code')
+        if(action == "Play Gif"):
+            if(code == "Treat"):
                 self.set_animation_state(AnimationStates.GIVE_TREAT)
+        elif(action == "Computer"):
+            if("Shutdown" in code):
+               os.system(f"shutdown /s /t {code[code.rfind("Shutdown ")+len("Shutdown "):]}") 
+
 
