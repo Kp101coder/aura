bf80b7de9cc55f097f3d0b406bb9fd0ded96de10
D Krish Prabhu <prabhukrish710@gmail.com>
2024-07-23 21:16:35 -0500

    suum

--- AuraBuddy/src/ActionHandler.py
+++ AuraBuddy/src/ActionHandler.py
@@ -21,10 +21,5 @@ class ActionHandler():
                 c.add_event(arr[0], arr[1], arr[2], arr[3], arr[5], arr[6])
         elif(action == "Computer"): 
             os.system(code)
-            # if(code == "Cancel Shutdown"):
-            #     os.system("shutdown -a")
-            # elif("Shutdown" in code):
-            #    sec = code[code.rfind("Shutdown ")+len("Shutdown "):]
-            #    os.system(f"shutdown /s /t {sec}")
 
 
